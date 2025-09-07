529c5b544d1e3883fa50c03058420d9973afde3c
Y-manisha <yadavmanisha767821@gmail.com>
2025-09-07 20:48:21 +0530

    Initial commit 1.1

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,13 +6,13 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level up
 data_path = os.path.join(BASE_DIR, 'Mall_Customers_model.pkl')
-model = joblib.load(Mall_Customers)
+model = joblib.load(data_path)
 
 
 
 class TestModelTraining(unittest.TestCase):
     def test_scaler(self):
-        # model = joblib.load('Mall_Customers_model.pkl')
+        # model = joblib.load(data_path)
         self.assertIsInstance(model, KMeans)  
         self.assertEqual(len(model.feature_importances_), 4)
 
