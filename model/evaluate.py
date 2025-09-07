b3180ee37eb30bc0686c43ce7a77ba2a3e9d3056
Y-manisha <yadavmanisha767821@gmail.com>
2025-09-07 12:24:51 +0530

    Initial commit 1.1

--- model/evaluate.py
+++ model/evaluate.py
@@ -6,7 +6,7 @@ from sklearn.metrics import silhouette_score
 import joblib
 
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level updata_path = os.path.join(BASE_DIR, 'data', 'Mall_Customer.csv')
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level updata_path = os.path.join(BASE_DIR, 'data', 'Mall_Customers.csv')
 
 # Load dataset
 df = pd.read_csv("data/Mall_Customers.csv")
@@ -14,7 +14,7 @@ X = df[["Annual Income (k$)", "Spending Score (1-100)"]]
 
 # Load saved model
 BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level updata_path = os.path.join(BASE_DIR, 'Mall_Customer_model.pkl')
-model = joblib.load(Mall_Customer)
+model = joblib.load(Mall_Customers)
 
 # Scale the data
 X_scaled = scaler.transform(X)
