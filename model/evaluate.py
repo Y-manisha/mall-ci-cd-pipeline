e0e5548606e8d63381259be0e597a5ae5e765c79
Y-manisha <yadavmanisha767821@gmail.com>
2025-09-07 12:57:39 +0530

    Initial commit 1.1

--- model/evaluate.py
+++ model/evaluate.py
@@ -6,15 +6,17 @@ from sklearn.metrics import silhouette_score
 import joblib
 
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level updata_path = os.path.join(BASE_DIR,'data', 'Mall_Customers.csv')
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level 
+data_path = os.path.join(BASE_DIR,'data', 'Mall_Customers.csv')
 
 # Load dataset
-df = pd.read_csv("data/Mall_Customers.csv")
+df = pd.read_csv(data_path)
 X = df[["Annual Income (k$)", "Spending Score (1-100)"]]
 
 # Load saved model
-BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level updata_path = os.path.join(BASE_DIR, 'Mall_Customer_model.pkl')
-model = joblib.load("data/Mall_Customers.csv")
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level 
+data_path = os.path.join(BASE_DIR, 'Mall_Customers_model.pkl')
+model = joblib.load(data_path)
 
 # Scale the data
 X_scaled = scaler.transform(X)
--- model/train.py
+++ model/train.py
@@ -4,9 +4,13 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import joblib
+import os 
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level 
+data_path = os.path.join(BASE_DIR, 'data', 'Mall_Customers.csv')
 
 # Load dataset
-df = pd.read_csv("data/Mall_Customers.csv")
+df = pd.read_csv(data_path)
 
 # Select two features
 X = df[["Annual Income (k$)", "Spending Score (1-100)"]]
@@ -20,4 +24,4 @@ model = KMeans(n_clusters=5, random_state=42)
 model.fit(X_scaled)
 
 # Save the model 
-model = joblib.dump(model, "Mall_Customer_model.pkl")
+model = joblib.dump(model, "Mall_Customers_model.pkl")
