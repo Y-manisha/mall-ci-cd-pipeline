1eacecdd1272544e6ba6979222cac6f450f99b76
Y-manisha <yadavmanisha767821@gmail.com>
2025-09-07 20:42:31 +0530

    Initial commit 1.1

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,6 @@
 
 import unittest
 import joblib
-from sklearn.preprocessing import StandardScale
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go one level up
@@ -12,10 +11,10 @@ model = joblib.load(Mall_Customer)
 
 
 class TestModelTraining(unittest.TestCase):
-    def test_model_training(self):
+    def test_scaler(self):
         # model = joblib.load('Mall_Customer_model.pkl')
         self.assertIsInstance(model, KMeans)  
-        self.assertGreaterEqual(len(model.feature_importances_), 4)
+        self.assertEqual(len(model.feature_importances_), 4)
 
 if __name__ == '__main__':
     unittest.main()
